f3831ca3bf86d6225f476a12287f1cc4c080868d
ivanpalachuk <ivanzar@hotmail.com>
2023-07-01 22:09:35 -0300

    [ADD] Menu Functionality and button for APP PEDIDO

---
+++ public/data/Data.jsx
@@ -0,0 +1,22 @@
+const Data = [
+
+    { url: "/images/burgerPNG/AtlanticaMax.png", name: "Atlantica Max", text: "Gigante como la costa, provoleta, morrones asados, lechuga con alioli de albahaca" },
+    { url: "images/burgerPNG/BigBox.png", name: "Big Box", text:"Si habia una mejor forma de hacer la salsa big era esta, nuestro toque es magia, proba y alegrate como con un gol de Messi"},
+    { url: "images/burgerPNG/BigPapooza.png", name: "Big Papooza", text:"Nuestra version 2.0 de la big, le metimos papas rejillas y papas adentro de la burger, para que no lo tengas que hacer vos" },
+    { url: "images/burgerPNG/BlueBurger.png", name: "Blue Burger", text:"Una burger sofisticada, para paladares particulares, nuestra agente 007" },
+    { url: "images/burgerPNG/Bondiola Braseada.png", name: "Bondeola Braseada", text:"Bondiola, desmenuzada, si, asi de tierna y encima tiene un toque ahumado" },
+    { url: "images/burgerPNG/Brodas.png", name: "Brodas",text:"Nuestro buque insignia, probala, senti el bacon, la salsa cuarto y luego podes aplaudir el tiempo que quieras" },
+    { url: "images/burgerPNG/Cheese.png", name: "Cheese", text:"La madre de todas las recetas, Cheeseburger, la leyenda hecha un plato" },
+    { url: "images/burgerPNG/chicken box.png", name: "Chicken Box", text:"Pollo frito... Listo, podria ser esa la descripcion, por si no alcanza, tiene bacon crujiente" },
+    { url: "images/burgerPNG/Criolla.png", name: "Criolla", text:"Si los gauchos en lugar de asados se hubieran echo famosos por sus hamburguesas seria por una como esta" },
+    { url: "images/burgerPNG/Fish Box.png", name: "Fish Box", text:"Algo mas liviano? El nutricionista te dijo no tanta carne?, tranqui aca estamos para zafarte" },
+    { url: "images/burgerPNG/Fried Onion.png", name: "Fried Onion",text:"Desde el vamos es doble,aplastamos adentro cebolla caramelizada mientras la cocinamos. Un monton" },
+    { url: "images/burgerPNG/Jack daniels.png", name: "Jack Daniel`s", text:"Un poco de whisky para caramelizar la cebolla, Papo estaria orgulloso de nosotros" },
+    { url: "images/burgerPNG/Mila 420.png", name: "Mila 420", text:"La mila de barrio, la que comiste siempre, pero con un toquecito ahumado" },
+    { url: "images/burgerPNG/Mila Box.png", name: "Mila Box",text:"Es como las milas que te hace tu abuela, pero con cebolla crispy y salsa Cuarto" },
+    { url: "images/burgerPNG/Tasty Smoke.png", name: "Tasty Smoke",text:"Cebolla picada, salsita ahumada, tomatito... Es lo que regalas para quedar bien con alguien" },
+    { url: "images/burgerPNG/tbt.png", name: "TBT", text:"Una explosion de quesos,cheddar, provo, bastoncitos, si fuese una peli seria Rapidos y Furiosos"},
+    { url: "images/burgerPNG/Veggie.png", name: "Veggie",text:"Por la razon que sea, obvio que aca tenes la opcion vegetariana" }
+]
+
+export default Data;
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,13 +1,7 @@
-const Menu = () => {
-  const burgersURL = [
-    { url: "/images/burgerPNG/AtlanticaMax.png", name: "Atlantica Max", text: "lorem ipsum" },
-    { url: "images/burgerPNG/BigBox.png", name: "Big Box" },
-    { url: "images/burgerPNG/BigPapooza.png", name: "Big Papooza" },
-    { url: "images/burgerPNG/BlueBurger.png", name: "BlueBurger" },
-    { url: "images/burgerPNG/Brodas.png", name: "Brodas" },
-    { url: "images/burgerPNG/tbt.png", name: "TBT" }
-  ]
+import Data from "/public/data/Data"
+import { Link } from "react-router-dom"
 
+const Menu = () => {
 
   return (
     <>
@@ -22,25 +16,26 @@ const Menu = () => {
           {/* Contenedor del grupo de productos */}
           <div className="mt-8 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 [perspective:1000px]">
 
-            {burgersURL.map((product, id) => (
+            {Data.map((product, id) => (
 
               <div key={id} className="group relative">
 
-                <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] bg-black">
+                <div className="relative h-full w-full rounded-xl shadow-xl p-4 transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] bg-black">
                   <img
                     src={product.url}
                     alt={product.imageAlt}
                     className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40"
                   />
-                  <div className="absolute inset-0 h-full w-full rounded-xl bg-black/80 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
-                    <div className='flex justify-center font-custom1 text-theboxyellow text-xl uppercase p-8'>{product.text}</div>
+                  <div className="absolute inset-0 h-full w-full rounded-xl bg-black/80 px-4 pb-4 text-center [transform:rotateY(180deg)] [backface-visibility:hidden] overflow-hidden flex flex-col justify-between">
+                    <div className='font-custom1 text-theboxyellow text-xl uppercase mt-8 mb-4'>{product.text}</div>
+                    <Link to="https://pedidos.theboxburgerculture.com.ar"><button className="text-theboxyellow font-custom1 uppercase text-m border-2 rounded-2xl p-2 border-current">Pedila aca y mira todos los ingredientes</button></Link>
                   </div>
                 </div>
 
 
                 <div className="mt-2 flex justify-center">
                   <div>
-                    <h3 className="text-2xl text-black text-center font-custom1">
+                    <h3 className="text-2xl text-black text-center font-custom1 uppercase font-extrabold">
                       {product.name}
                     </h3>
                   </div>
