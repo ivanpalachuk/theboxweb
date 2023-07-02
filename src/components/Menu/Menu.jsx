b097f684e4dfd01f54bdb6f96854a1d54965d1a5
ivanpalachuk <ivanzar@hotmail.com>
2023-07-01 23:14:56 -0300

    [EDIT] remove badfunction

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,33 +1,25 @@
-import React, { useState } from "react";
-import Data from "/public/data/Data";
-import { Link } from "react-router-dom";
+import Data from "/public/data/Data"
+import { Link } from "react-router-dom"
 
 const Menu = () => {
-  const [rotate, setRotate] = useState(false); // Estado inicial para controlar el giro
-
-  const handleCardClick = () => {
-    setRotate(!rotate); // Cambiar el estado de giro al hacer clic en la tarjeta
-  };
 
   return (
     <>
-      {/* Fondo de la pagina */}
+
+      {/* Fondo de la pagina   */}
       <div className="bg-theboxyellow">
         {/* Contenedor de titulo y grupo */}
         <div className="mx-auto max-w-2xl px-4 py-8 pb-12 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           {/* Titulo */}
-          <h2 className="text-2xl font-bold text-center text-black font-custom1 border-b-2 border-current mt-12">
-            NUESTRAS CREACIONES
-          </h2>
+          <h2 className="text-2xl font-bold text-center text-black font-custom1 border-b-2 border-current mt-12">NUESTRAS CREACIONES</h2>
 
           {/* Contenedor del grupo de productos */}
           <div className="mt-8 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 [perspective:1000px]">
+
             {Data.map((product, id) => (
-              <div
-                key={id}
-                className={`group relative ${rotate ? "rotate" : ""}`}
-                onClick={handleCardClick}
-              >
+
+              <div key={id} className="group relative">
+
                 <div className="relative h-full w-full rounded-xl shadow-xl p-4 transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] bg-black">
                   <img
                     src={product.url}
@@ -40,6 +32,7 @@ const Menu = () => {
                   </div>
                 </div>
 
+
                 <div className="mt-2 flex justify-center">
                   <div>
                     <h3 className="text-2xl text-black text-center font-custom1 uppercase font-extrabold">
@@ -48,12 +41,15 @@ const Menu = () => {
                   </div>
                 </div>
               </div>
+
+
+
             ))}
+
           </div>
         </div>
-      </div>
+      </div >
     </>
-  );
-};
-
-export default Menu;
+  )
+}
+export default Menu
