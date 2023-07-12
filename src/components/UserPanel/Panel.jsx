99c53ccb7a7e16650a5574bfa38f184f22ec13a6
ivanpalachuk <ivanzar@hotmail.com>
2023-07-12 18:29:00 -0300

    [EDIT] change panel message

--- src/components/UserPanel/Panel.jsx
+++ src/components/UserPanel/Panel.jsx
@@ -8,7 +8,7 @@ const Panel = () => {
         <div className="h-screen bg-black [word-spacing:3px] text-theboxyellow  font-custom1 ">
             <h1 className=" uppercase text-3xl mx-auto w-[90%] text-center p-4 relative top-16">Bienvenido a la Comunidad</h1>
             <div className="greetings text-lg w-10/12 m-auto p-8  h-auto rounded-xl relative top-16 text-center">
-                Hola, {user.displayName} en breve vas a ver todas las novedades y ventajas que tiene sumarse a la comunidad y cultura de THEBOX, estate atento para recibir las novedades
+                Hola, {user.displayName} ya estas participando por el sorteo del dia del amigo! recorda que en nuestro instagram tenes las bases y condiciones, pronto vas a estar recibiendo las novedades y beneficios de ser parte de esta comunidad!!!
             </div>
         </div>
     );
